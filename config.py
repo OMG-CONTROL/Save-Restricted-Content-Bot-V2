64a8d5dc6a2efb95651c9009717c0e3cae75963e
OMG-CONTROL <a1k2v3v3k2a1@gmail.com>
2024-10-12 20:32:30 +0530

    config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+API_ID = int(getenv("API_ID", "22542407"))
+API_HASH = getenv("API_HASH", "e162d195c0ab0f1a45f4329bc02e6b4e")
+BOT_TOKEN = getenv("BOT_TOKEN", "7564201003:AAGEaICEKcB1JlH3vUwCdJ1G51jDkB8Q4No")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7524828572").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+LOG_GROUP = getenv("LOG_GROUP", "1002230302878")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "1002230302878"))
